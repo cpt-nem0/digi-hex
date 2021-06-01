80783ac7ff75184adfc2e7868b283a118eeff908
cpt-nem0 <rohanya.76426@gmail.com>
2021-06-02 03:29:36 +0530

    updated init file

--- ledger/__init__.py
+++ ledger/__init__.py
@@ -1,5 +1,11 @@
+import os
+
 from flask import Flask, app
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+load_dotenv()
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 from ledger import routes
