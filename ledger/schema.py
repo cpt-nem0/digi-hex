09b9f7b095345487af2a5582a37d0e98cc2fbcf5
cpt-nem0 <rohanya.76426@gmail.com>
2021-06-04 04:44:03 +0530

    added password hash functions

--- ledger/schema.py
+++ ledger/schema.py
@@ -1,4 +1,11 @@
 from ledger import mongo as db
+from ledger import bcrypt
+from ledger import login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return businessSchema.objects(user_id=user_id)
 
 
 class pendingTransactionsSchema(db.Document):
@@ -25,11 +32,23 @@ class clientSchema(db.EmbeddedDocument):
     clientTransactions = db.ListField(db.EmbeddedDocumentField('transactionSchema'))
 
 
-class businessSchema(db.Document):
+class businessSchema(db.Document, UserMixin):
     b_id = db.StringField(required=True, unique=True)
     b_name = db.StringField(required=True, unique=True)
     b_owner = db.StringField(required=True)
     b_email = db.StringField(required=True, unique=True)
     b_mobile = db.IntField(required=True, unique=True)
-    b_password = db.StringField(required=True)
+    b_password_hash = db.StringField(required=True)
     clients = db.ListField(db.EmbeddedDocumentField('clientSchema'))
+
+    @property
+    def b_password(self):
+        return self.b_password_hash
+
+    @b_password.setter
+    def b_password(self, plain_text_password):
+        self.b_password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+
+
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.b_password_hash, attempted_password)
