b3574d1f844adeabe32f5d675542048c75c6a6a4
cpt-nem0 <rohanya.76426@gmail.com>
2021-06-04 04:44:37 +0530

    improved user login

--- ledger/routes.py
+++ ledger/routes.py
@@ -1,8 +1,9 @@
 import uuid 
 
-from ledger import app, mongo
+from ledger import app
 from ledger.schema import *
 from flask import render_template, redirect, url_for, session, request, flash, get_flashed_messages
+from flask_login import login_user
 from ledger.forms import RegistrationForm, LoginForm
 
 
@@ -17,19 +18,20 @@ def dashboard():
     
     return render_template('landing_page.html')
 
-@app.route('/login/', methods=['POST', 'GET'])
+@app.route('/login/', methods=['GET', 'POST'])
 def login_page():
     form = LoginForm()
 
-    if request.method == 'POST' and form.validate_on_submit():
-
-        login_user = businessSchema.objects(b_email=form.user_email.data).first()
-        if login_user:
-            if form.user_password.data == login_user.b_password:
-                session['user'] = login_user.b_owner
-            else:
-                flash('Wrong Password')
-            return render_template('dashboard.html')
+    if form.validate_on_submit():
+        attempted_user = businessSchema.objects(b_email=form.user_email.data).first()
+        if attempted_user and attempted_user.check_password_correction(attempted_password=form.user_password.data):
+            login_user(attempted_user)
+            session['user'] = attempted_user.b_owner
+            session['user_id'] = attempted_user.b_id
+            print(attempted_user.b_owner)
+            return redirect(url_for('dashboard'))
+        else:
+            flash("Email and Password don't match. Try again")
 
     return render_template('login.html', form=form)
 
@@ -46,10 +48,11 @@ def register_page():
             b_owner = form.owner_name.data,
             b_email = form.owner_email.data,
             b_mobile =  form.owner_phone.data,
-            b_password = form.password.data
         )
+        newUser.b_password = form.password.data
         newUser.save()
         session['user'] = form.owner_name.data
+        session['user_id'] = newUser.b_id
         return redirect(url_for('dashboard'))
 
     if form.errors != {}: # errors from the validation
@@ -60,21 +63,35 @@ def register_page():
 
 @app.route('/paymentrequest/')
 def payment_request():
-    return render_template('payment_request.html')
+    if 'user' in session:
+        return render_template('payment_request.html')
+    
+    return render_template('landing_page.html')
 
 @app.route('/profile/')
 def user_profile():
-    return render_template('user_profile.html')
+    if 'user' in session:
+        user_data = businessSchema.objects(b_id=session['user_id']).first()
+        return render_template('user_profile.html', user_data=user_data)
+    
+    return render_template('landing_page.html')
 
 @app.route('/viewclients/')
 def view_clients():
-    return render_template('view_clients.html')
+    if 'user' in session:
+        return render_template('view_clients.html')
+
+    return render_template('landing_page.html')
 
 @app.route('/addclients/')
 def add_clients():
-    return render_template('add_clients.html')
+    if 'user' in session:
+        return render_template('add_clients.html')
+    
+    return render_template('landing_page.html')
 
 @app.route('/logout/')
 def logout():
     session.pop('user', None)
+    session.pop('user_id', None)
     return redirect(url_for('landing_page'))
