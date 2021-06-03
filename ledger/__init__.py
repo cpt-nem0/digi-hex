e4beb9afc889d0a2dfa6e86ea6f03cc3e1c75e6d
cpt-nem0 <rohanya.76426@gmail.com>
2021-06-04 02:07:53 +0530

    mongoengine config

--- ledger/__init__.py
+++ ledger/__init__.py
@@ -1,11 +1,17 @@
 import os
 
 from flask import Flask, app
+from flask_mongoengine import MongoEngine
 from dotenv import load_dotenv
 
 app = Flask(__name__)
 load_dotenv()
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['MONGODB_HOST'] = ''.join(os.getenv('MONGO_URI')+os.getenv('DB_NAME'))
+app.config['MONGODB_CONNECT'] = False
+
+mongo = MongoEngine()
+mongo.init_app(app)
 
 from ledger import routes
