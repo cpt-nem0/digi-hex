e4751bf6f599a6322abdcdef864c2bcd3edd6bab
cpt-nem0 <rohanya.76426@gmail.com>
2021-06-04 04:42:35 +0530

    added new configs

--- ledger/__init__.py
+++ ledger/__init__.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask, app
 from flask_mongoengine import MongoEngine
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv
 
 app = Flask(__name__)
@@ -13,5 +15,9 @@ app.config['MONGODB_CONNECT'] = False
 
 mongo = MongoEngine()
 mongo.init_app(app)
+
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
 
 from ledger import routes
