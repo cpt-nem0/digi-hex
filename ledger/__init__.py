cab572112770800e5881f2f9af8df2b66ce4d528
cpt-nem0 <rohanya.76426@gmail.com>
2021-06-06 03:30:32 +0530

    added config and handled exceptions

--- ledger/__init__.py
+++ ledger/__init__.py
@@ -4,20 +4,27 @@ from flask import Flask, app
 from flask_mongoengine import MongoEngine
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv
 
 app = Flask(__name__)
 load_dotenv()
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['MONGODB_HOST'] = ''.join(os.getenv('MONGO_URI')+os.getenv('DB_NAME'))
+app.config['MONGODB_HOST'] = os.getenv('MONGO_URI') # remember to add DB here
 app.config['MONGODB_CONNECT'] = False
 
-mongo = MongoEngine()
-mongo.init_app(app)
+try: 
+    mongo = MongoEngine()
+    mongo.init_app(app)
+    print('Database connected!!')
+
+except:
+    print('Database connection failure')
 
 bcrypt = Bcrypt(app)
 
 login_manager = LoginManager(app)
+message = MIMEMultipart()
 
 from ledger import routes
