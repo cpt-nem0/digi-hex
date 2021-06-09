e840ab04b16ca6eb5286091a34592273bd794379
Rohan Yadav <48692923+cpt-nem0@users.noreply.github.com>
2021-06-09 22:21:18 +0530

    Update schema.py

--- ledger/schema.py
+++ ledger/schema.py
@@ -28,8 +28,8 @@ class Transaction(db.EmbeddedDocument):
 
 class Clients(db.EmbeddedDocument):
     clientName = db.StringField(sparse=True)
-    clientEmail = db.StringField(sparse=True, unique=True)
-    clientMobile = db.IntField(sparse=True, unique=True)
+    clientEmail = db.StringField(sparse=True)
+    clientMobile = db.IntField(sparse=True)
     firstTransaction = db.IntField(sparse=True)
     clientTransactions = db.ListField(db.EmbeddedDocumentField('Transaction'))
 
